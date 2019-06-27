707521f00d8563c6a58f07fb6248abf6a7466379
Stouse49 <Stouse49@gmail.com>
2019-06-27 13:59:50 -0700

    Rename BTC to GLC

--- core/src/test/java/org/bitcoinj/core/CoinTest.java
+++ core/src/test/java/org/bitcoinj/core/CoinTest.java
@@ -122,10 +122,10 @@ public class CoinTest {
 
     @Test
     public void testToFriendlyString() {
-        assertEquals("1.00 BTC", COIN.toFriendlyString());
-        assertEquals("1.23 BTC", valueOf(1, 23).toFriendlyString());
-        assertEquals("0.001 BTC", COIN.divide(1000).toFriendlyString());
-        assertEquals("-1.23 BTC", valueOf(1, 23).negate().toFriendlyString());
+        assertEquals("1.00 GLC", COIN.toFriendlyString());
+        assertEquals("1.23 GLC", valueOf(1, 23).toFriendlyString());
+        assertEquals("0.001 GLC", COIN.divide(1000).toFriendlyString());
+        assertEquals("-1.23 GLC", valueOf(1, 23).negate().toFriendlyString());
     }
 
     /**
